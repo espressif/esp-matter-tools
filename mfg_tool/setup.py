a7fa6506699b2a5ac8f38379b480f79a59e9781a
mahesh <maheshpimple2002@gmail.com>
2026-02-17 11:38:52 +0530

    mfg-tool: remove pkg_resources dependency, apply patch changes

--- mfg_tool/setup.py
+++ mfg_tool/setup.py
@@ -4,48 +4,7 @@
 
 import os
 import sys
-import subprocess
 from setuptools import setup, find_packages
-from setuptools.command.build_py import build_py
-import pkg_resources
-
-patch_list = [
-{
-    'package': 'deps',
-    'file': 'patches/0001-mfg-gen-no-bin-and-replace-print-by-logging.patch'
-}]
-
-def is_patch_applied(patch_file):
-    try:
-        result = subprocess.run(["patch", "--dry-run", "-p2"], input=open(patch_file, "rb").read(),
-                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
-        return b'previously applied' in result.stderr or result.returncode != 0
-    except Exception as e:
-        print(f"Error checking patch status: {e}")
-        return False
-
-def apply_patch(pkg_name, file_name):
-    patch_file = pkg_resources.resource_filename(pkg_name, file_name)
-
-    if is_patch_applied(patch_file):
-        print("Patch already applied, skipping.")
-        return
-
-    try:
-        subprocess.run(["patch", "-p2"], input=open(patch_file, "rb").read(), check=True)
-        print("Patch applied successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error applying patch: {e}")
-        sys.exit(1)
-
-class CustomBuild(build_py):
-    def run(self):
-        for patch in patch_list:
-            print(f" Applying patch {patch['file']} before building the package ")
-            # Apply patch before building
-            apply_patch(patch['package'], patch['file'])
-        # Continue with normal build process
-        super().run()
 
 
 def get_install_requires():
@@ -116,13 +75,9 @@ setup(
     install_requires = get_install_requires(),
     include_package_data = True,
     packages = find_packages(),
-    package_data = {
-      'deps':['patches/*.patch'],
-    },
     entry_points={
         'console_scripts': [
             'esp-matter-mfg-tool = sources.mfg_tool:main',
         ],
     },
-    cmdclass={"build_py": CustomBuild},
 )
